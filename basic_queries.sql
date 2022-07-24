4d491c01a6923593d80252b629e67bb277a43eed
Ahmad-E-Nabeel <94029139+Ahmad-E-Nabeel@users.noreply.github.com>
2022-07-24 12:37:57 -0400

    Add files via upload

---
+++ basic_queries.sql
@@ -0,0 +1,24 @@
+SELECT * FROM patient;
+SELECT * FROM patient WHERE name = "Coco";
+SELECT * FROM patient WHERE Patient_Id = 2;
+
+SELECT * FROM owner;
+SELECT * FROM owner WHERE name = "Jason J Penrod";
+SELECT * FROM owner WHERE Owner_Id = 4;
+
+SELECT * FROM employee;
+SELECT * FROM employee WHERE name = "Jacob J Talkington";
+SELECT * FROM employee WHERE SIN = "760436888";
+
+SELECT * FROM dvm;
+SELECT * FROM dvm WHERE Specialization = "Dentist";
+
+SELECT * FROM staff;
+SELECT * FROM staff WHERE Staff_Id = "666433072";
+
+SELECT * FROM action;
+SELECT * FROM action WHERE Code = "FUSO";
+
+SELECT * FROM item;
+SELECT * FROM treatment;
+SELECT * FROM `condition`;
