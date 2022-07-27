1f9168cb46691d68283e9108e148b31b8e20d7fd
Lorenzo Quiambao <46307552+ltquiambao@users.noreply.github.com>
2022-07-26 23:52:44 -0400

    Update setup_tables.sql

--- setup_tables.sql
+++ setup_tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE `OWNER` (
     `Owner_Id` INT NOT NULL,
     `Name` VARCHAR(50) NOT NULL,
     `Street_Number` VARCHAR(10),
-    `Street_Name` VARCHAR(20) NOT NULL,   
+    `Street_Name` VARCHAR(20) NOT NULL,
     `City` VARCHAR(20) NOT NULL,
     `Province` VARCHAR(20) NOT NULL,
     `Postal_Code` CHAR(7) NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE `PATIENT` (
     `Species` ENUM('Feline', 'Canine') NOT NULL,
     `Breed` VARCHAR(30),
     `Birthdate` DATE NOT NULL CHECK (`Birthdate` > '1990-01-01'),
-    `Weight` DEC(4,2) NOT NULL CHECK (`Weight` > 00.00 AND `Weight` < 99.00),
+    `Weight` DEC(4 , 2 ) NOT NULL CHECK (`Weight` > 00.00 AND `Weight` < 99.00),
     `Color_Mark` VARCHAR(30),
     `Microchip` CHAR(15),
     `Rabies_Tag` CHAR(8),
@@ -27,7 +27,7 @@ CREATE TABLE `PATIENT` (
     FOREIGN KEY (`Primary_Owner`)
         REFERENCES `OWNER` (`Owner_Id`)
         ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (`Secondary_Owner`)
+    FOREIGN KEY (`Secondary_Owner`)
         REFERENCES `OWNER` (`Owner_Id`)
         ON DELETE SET NULL ON UPDATE CASCADE,
     UNIQUE KEY (`Microchip`),
@@ -81,7 +81,7 @@ CREATE TABLE `EMPLOYEE` (
 
 CREATE TABLE `DVM` (
     `DVM_Id` CHAR(9) NOT NULL,
-    `Specialization` VARCHAR(20),
+    `Specialization` VARCHAR(20) DEFAULT 'Regular',
     PRIMARY KEY (`DVM_Id`),
     FOREIGN KEY (`DVM_Id`)
         REFERENCES `EMPLOYEE` (`SIN`)
@@ -100,13 +100,13 @@ CREATE TABLE `APPOINTMENT` (
     `Appoint_Id` INT NOT NULL,
     `Patient_Id` INT NOT NULL,
     `DVM_Id` CHAR(9) NOT NULL,
-    `Staff_Id` CHAR(9) NOT NULL,
-    `Reason` VARCHAR(50),
+    `Staff_Id` CHAR(9),
+    `Reason` VARCHAR(50) DEFAULT 'General checkup',
     `Schedule` DATETIME NOT NULL,
     PRIMARY KEY (`Patient_Id` , `Schedule`),
     FOREIGN KEY (`Staff_Id`)
         REFERENCES `STAFF` (`Staff_Id`)
-        ON DELETE NO ACTION ON UPDATE CASCADE,
+        ON DELETE SET NULL ON UPDATE CASCADE,
     FOREIGN KEY (`DVM_Id`)
         REFERENCES `DVM` (`DVM_Id`)
         ON DELETE CASCADE ON UPDATE CASCADE,
@@ -118,7 +118,7 @@ CREATE TABLE `APPOINTMENT` (
 
 CREATE TABLE `CONDITION_DIAGNOSED` (
     `Appoint_Id` INT NOT NULL,
-    `Cond_code` VARCHAR(8) NOT NULL,
+    `Cond_code` VARCHAR(8) NOT NULL DEFAULT 'Hy',
     `Notes` VARCHAR(30),
     PRIMARY KEY (`Appoint_Id` , `Cond_code`),
     FOREIGN KEY (`Appoint_Id`)
@@ -131,8 +131,8 @@ CREATE TABLE `CONDITION_DIAGNOSED` (
 
 CREATE TABLE `ITEM_TREATMENT_PRESCRIBED` (
     `Appoint_Id` INT NOT NULL,
-    `Item_code` VARCHAR(8) NOT NULL,
-    `Treat_code` VARCHAR(8) NOT NULL,
+    `Item_code` VARCHAR(8) NOT NULL DEFAULT 'na',
+    `Treat_code` VARCHAR(8) NOT NULL DEFAULT 'na',
     `Dosage_amount` VARCHAR(8),
     `Method` VARCHAR(10),
     `Frequency` VARCHAR(10),
